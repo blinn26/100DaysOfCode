372f7133ef08a5fa0dabb6cb177ffdef8dd1af4c
createdbybenlinn <ben@benlinn.com>
2023-08-15 15:05:24 -0700

    turtle

---
+++ day-18-1/main.py
@@ -0,0 +1,17 @@
+######## Challenge 1 - Draw a Square ############
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+
+timmy_the_turtle = t.Turtle()
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+t.done()
