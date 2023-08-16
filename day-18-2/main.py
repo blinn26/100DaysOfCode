1912d965438c198b0b96d222a55a2c3f0fbf306c
createdbybenlinn <ben@benlinn.com>
2023-08-15 22:14:04 -0700

    step 2

---
+++ day-18-2/main.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
