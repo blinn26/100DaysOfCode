45715e0348aa06127dfb270827323d060c77eb3f
createdbybenlinn <ben@benlinn.com>
2023-08-24 08:14:55 -0700

    fixing txt and csv

--- day-24-end/main.py
+++ day-24-end/main.py
@@ -1,17 +1,17 @@
-#Reading Files
+import os
 
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# Check if 'my_file.txt' exists. If so, read and print its content.
+if os.path.exists("my_file.txt"):
+    with open("my_file.txt") as file:
+        contents = file.read()
+        print(contents)
+else:
+    print("my_file.txt does not exist.")
 
-#Writing to Files
+# Append text to 'my_file.txt' without overwriting its existing content.
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew text.")
 
-with open("my_file.txt", mode="w") as file:
-    file.write("New text.")
-    
-#Opening a File that doesn't exit in write mode will create it from scratch
-
+# Create (or overwrite) 'file_that_doesnt_exist.txt' with the content "New text."
 with open("file_that_doesnt_exist.txt", mode="w") as file:
     file.write("New text.")
-
-
