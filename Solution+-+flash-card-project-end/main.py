ffa763535e8ab081f794bd031302f74b927a0023
createdbybenlinn <ben@benlinn.com>
2023-08-24 22:53:34 -0700

    main

--- Solution+-+flash-card-project-end/main.py
+++ Solution+-+flash-card-project-end/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pandas
 import random
 
+
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
@@ -50,22 +51,22 @@ canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(
+    400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button = Button(
+    image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button = Button(
+    image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
 
 window.mainloop()
-
-
-
