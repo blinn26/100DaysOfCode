14983cb8042cd13b50d1ed0ce07c38536edb313b
createdbybenlinn <ben@benlinn.com>
2023-08-27 14:14:13 -0700

    example correction on the example

--- Solution+-+password-manager-end/main.py
+++ Solution+-+password-manager-end/main.py
@@ -7,8 +7,11 @@ import pyperclip
 
 
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ]
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -23,8 +26,8 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
 
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
     website = website_entry.get()
@@ -35,8 +38,10 @@ def save():
         messagebox.showinfo(
             title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                              f"\nPassword: {password} \nIs it ok to save?")
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?"
+        )
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
@@ -45,20 +50,24 @@ def save():
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
+logo_img = PhotoImage(
+    file="/Users/benlinn/Python Course/100DaysOfCode/Solution+-+password-manager-end/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
+
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
+
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
@@ -66,9 +75,11 @@ password_label.grid(row=3, column=0)
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
+
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.insert(0, "email@example.com")
+
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -76,6 +87,7 @@ password_entry.grid(row=3, column=1)
 generate_password_button = Button(
     text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
+
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
