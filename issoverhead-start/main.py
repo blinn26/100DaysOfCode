873bbf4621c33a450161b14ce733b685c19de403
createdbybenlinn <ben@benlinn.com>
2023-09-03 13:39:25 -0700

    entered long and lat

--- issoverhead-start/main.py
+++ issoverhead-start/main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 33.556660 # Your latitude
+MY_LONG = -117.723500 # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
