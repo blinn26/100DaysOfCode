f950b8d142c1f7045c70fe3657da773e0bd058da
createdbybenlinn <ben@benlinn.com>
2023-09-17 14:46:28 -0700

    day 37

--- Day 37/main.py
+++ Day 37/main.py
@@ -1,35 +1,50 @@
 import os
 import requests
+from datetime import datetime
 
 # Fetch USERNAME and TOKEN from environment variables
-USERNAME = os.environ.get('PIXELA_USERNAME')
-TOKEN = os.environ.get('PIXELA_TOKEN')
+USERNAME = os.getenv('PIXELA_USERNAME')
+TOKEN = os.getenv('PIXELA_TOKEN')
 
-pixela_endpoint = "https://pixe.la/v1/users"
+if not USERNAME or not TOKEN:
+    print("Environment variables for USERNAME and TOKEN are not set!")
+    exit(1)
 
-user_params = {
-  "token": TOKEN,
-  "username": USERNAME,
-  "agreeTermsOfService": "yes",
-  "notMinor": "yes",
-}
+# Define a constant for the graph ID
+GRAPH1 = "graph1"
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# Base endpoint for Pixela
+pixela_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH1}"
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-  "id": "graph1",
-  "name": "Cycling Graph",
-  "unit": "Km",
-  "type": "float",
-  "color": "ajisai",
-}
-
+# Header for authentication
 headers = {
-  "X-USER-TOKEN": TOKEN
+    "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
+# Dates
+day1 = datetime(year=2023, month=9, day=15)
+day2 = datetime(year=2023, month=9, day=16)
+
+# Data
+pixel_data_day1 = {"date": day1.strftime("%Y%m%d"), "quantity": "4"}
+pixel_data_day2 = {"date": day2.strftime("%Y%m%d"), "quantity": "5"}
+
+# POST for day1
+response1 = requests.post(url=pixela_endpoint, json=pixel_data_day1, headers=headers)
+print(f"Response for day1: {response1.text}")
+
+# POST for day2
+response2 = requests.post(url=pixela_endpoint, json=pixel_data_day2, headers=headers)
+print(f"Response for day2: {response2.text}")
+
+# Today
+today = datetime.now()
+
+# PUT for today
+update_endpoint = f"{pixela_endpoint}/{today.strftime('%Y%m%d')}"
+update_response = requests.put(url=update_endpoint, json={"quantity": "8"}, headers=headers)
+print(f"Update response: {update_response.text}")
+
+# GET for today
+response = requests.get(url=update_endpoint, headers=headers)
+print(f"Response: {response.text}")
