cd858a353ef18f78e2111dac9ab42106f2f467b5
createdbybenlinn <ben@benlinn.com>
2023-09-24 21:46:12 -0700

    clean up

--- stock-news-hard-start/main.py
+++ stock-news-hard-start/main.py
@@ -8,7 +8,7 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 ## STEP 1: Use https://newsapi.org/docs/endpoints/everything
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+#HINT 2: Work out the value of 5% of yesterday's closing stock price. 
 
 
 
